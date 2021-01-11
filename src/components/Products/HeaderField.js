16780f653b5416b7c0abbc17825460f4df11bf67
fsantos22 <22fsantos@gmail.com>
2021-01-11 05:19:56 -0300

    modo lojista finalizado

--- src/components/Products/HeaderField.js
+++ src/components/Products/HeaderField.js
@@ -6,30 +6,61 @@ const ProductHeader = styled.section`
   padding: 10px;
   background-color: #000000;
   color: #ffffff;
+  align-items: center;
 
   p {
-    margin: 0 15px 0 130px;
+    margin: ${(props) => props.HeaderMargin};
+    width: 50%;
+  }
+
+  button {
+    background-color: #000000;
+    color: #ffffff;
+    height: 35px;
+    width: 100px;
+    border-radius: 5px;
+    margin: 0 10px;
+  }
+
+  button:hover {
+    background-color: orange;
+    transition: ease 0.3s;
   }
 
   @media (max-width: 409px) {
-    flex-direction: column;
-    margin-left: 120px;
+    flex-direction: row;
+    /* margin-left: 120px; */
     align-items: center;
 
-    p{
+    p {
       margin: 0;
+      display: ${props => props.pDisplay};
     }
   }
 `;
-
-export const HeaderField = (props) => {
-  return (
-    <ProductHeader>
-      <p>Total: {props.quantity} itens</p>
-      <select onChange={props.orderType}>
-        <option value="a-z">Ordem crescente</option>
-        <option value="z-a">Ordem decrescente</option>
-      </select>
-    </ProductHeader>
-  );
+export class HeaderField extends React.Component {
+  render() {
+    let Modo;
+    if (this.props.Modo) {
+      Modo = "Modo cliente";
+      return (
+        <ProductHeader>
+          <p>Total: {this.props.quantity} itens</p>
+          <button onClick={this.props.ChangeModeStatus}>{Modo}</button>
+        </ProductHeader>
+      );
+    } else {
+      Modo = "Modo lojista";
+      return (
+        <ProductHeader pDisplay={"none"}>
+          <p>Total: {this.props.quantity} itens</p>
+          <button onClick={this.props.ChangeModeStatus}>{Modo}</button>
+          <select onChange={this.props.orderType}>
+            <option value="a-z">Ordem crescente</option>
+            <option value="z-a">Ordem decrescente</option>
+          </select>
+        </ProductHeader>
+      );
+    }
+  }
 }
